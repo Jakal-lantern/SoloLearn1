341ca8a0df6f4fa10231190f5cf3565f3af733ca
Sarene_Alexander <Sarene_Alexander@edu.mcctc.net>
2020-08-27 13:19:17 -0400

    Add project files.

---
+++ SoloLearn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int num;
+            num = 42;
+
+            bool a = false;
+            double b = 4.22;
+            string c = "Hi";
+            int d = 11;
+        }
+    }
+}
